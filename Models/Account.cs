6aee625f13b3d9fc801ca6183523cfdc468742ff
Cường Nguyễn Đức <chuoidochoduoi7e@gmail.com>
2025-06-02 17:45:52 +0700

    Add project files.

---
+++ Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace firstWeb.Models
+{
+    public class Account
+    {
+        public required string Id { get; set; }
+
+        public required string AccountName { get; set; }
+        public required string AccountPassword { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace firstWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
